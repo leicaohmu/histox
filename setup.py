72d1877b2222e5588f163b3bf19488f628efbd0c
leicaohmu <leicaohmu@gmail.com>
2026-06-23 01:12:51 -0400

    release: bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="histox",
-    version="0.2.0",
+    version="0.2.1",
     author="Lei Cao",
     author_email="caolei@hrbmu.edu.cn",
     description="Deep learning tools for digital histology",
